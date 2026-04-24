4c4f0e7816a5fb6f3c4f3d60d09fa79919a1efae
Muhammad Hamza <hamzahs2004@gmail.com>
2026-04-25 00:19:54 +0500

    feat: enhance Navbar with dynamic link to Compare page for Analyzer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import gitAtlasLogo from "/src/assets/images/gitatlas-logo.png";
+import { useLocation, matchPath, Link } from "react-router";
 
 function Navbar() {
+  let location = useLocation()
+  let isAnalyzerPage = matchPath("/analyzer/:username", location.pathname)
+
   return (
-    <div className={`w-full h-13 bg-white px-4`}>
+    <div className={`w-full h-13 bg-white px-4 flex items-center justify-between`}>
       <div className={`h-full w-fit flex items-center`}>
         <img className={`h-4.5 w-auto`} src={gitAtlasLogo} alt="logo" />
       </div>
+
+      {isAnalyzerPage && 
+        <Link className={`bg-[#252525] text-white text-[0.85rem] px-4 py-2 rounded-xl active:scale-95 transition-transform ease-in-out duration-100`}>
+          Compare
+        </Link>
+      }
     </div>
   );
 }
