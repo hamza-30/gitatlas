1d80e00bfb9f2d316d134187af3581a7b4c86912
Muhammad Hamza <hamzahs2004@gmail.com>
2026-04-24 19:01:45 +0500

    feat: implement Layout component and set up routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home'
+import Analyzer from './pages/Analyzer'
+import Compare from './pages/Compare'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Layout from './Layout'
+
+let router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "analyzer/:username",
+          element: <Analyzer />,
+        }, 
+        {
+          path: "compare",
+          element: <Compare />,
+        }
+      ]
+    }
+  ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
