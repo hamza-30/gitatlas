7905db01ebb23b84974217bcc1152b999f0e437d
Muhammad Hamza <hamzahs2004@gmail.com>
2026-04-25 22:08:24 +0500

    feat: enhance LanguageInfo component with border and padding for improved layout

--- src/components/LanguageInfo.jsx
+++ src/components/LanguageInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LanguageInfo() {
   return (
-    <div className={`flex justify-between text-gray-800`}>
+    <div className={`flex justify-between text-gray-800 border-b border-b-gray-100 pb-0.5`}>
         <div className={`flex items-center gap-x-1.5`}>
             <div className={`h-[0.88rem] w-[0.88rem] bg-amber-300 rounded-sm mb-0.5`}></div>
             <div className={`text-sm`}>JavaScript</div>
