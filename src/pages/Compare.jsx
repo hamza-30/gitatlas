ea0ccb872f06f9bbc3d4c99c1504c3356ef56d5a
Muhammad Hamza <hamzahs2004@gmail.com>
2026-05-06 01:49:35 +0500

    feat: implement user profile comparison in Compare page with API integration

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UsernameInput from "../components/UsernameInput";
 import { MdCompareArrows } from "react-icons/md";
 import ProfileCard from "../components/ProfileCard";
@@ -6,6 +6,42 @@ import ProfileCard from "../components/ProfileCard";
 function Compare() {
   const [user1, setUser1] = useState("");
   const [user2, setUser2] = useState("");
+  const [user1Profile, setUser1Profile] = useState(null);
+  const [user2Profile, setUser2Profile] = useState(null);
+  const [fetchError, setFetchError] = useState(false);
+  const [loading, setLoading] = useState(false)
+
+  function onCompareClick() {
+    if (user1 && user2) {
+      async function getUsersProfile() {
+        try {
+          setFetchError(false);
+          setUser1Profile(null);
+          setUser2Profile(null);
+
+          const [user1ProfileResult, user2ProfileResult] = await Promise.all([
+            fetch(`https://api.github.com/users/${user1}`),
+            fetch(`https://api.github.com/users/${user2}`),
+          ]);
+
+          if (!user1ProfileResult.ok || !user2ProfileResult.ok) {
+            throw new Error("Failed to fetch users data.");
+          }
+
+          let user1ProfileData = await user1ProfileResult.json();
+          let user2ProfileData = await user2ProfileResult.json();
+
+          setUser1Profile(user1ProfileData);
+          setUser2Profile(user2ProfileData);
+        } catch (error) {
+          setFetchError(true);
+          console.log(error);
+        }
+      }
+
+      getUsersProfile();
+    }
+  }
 
   return (
     <div
@@ -41,6 +77,7 @@ function Compare() {
 
           <button
             className={`bg-black text-white w-fit px-4 py-1.5 flex items-center justify-center gap-x-1 active:scale-95 transition-transform ease-out duration-150 rounded-sm`}
+            onClick={onCompareClick}
           >
             <MdCompareArrows className={`text-[1.3rem]`} />
             <p className={`text-[0.88rem]`}>Compare</p>
@@ -51,9 +88,31 @@ function Compare() {
       <div
         className={`w-full h-fit flex flex-col md:flex-row gap-y-2.5 gap-x-2`}
       >
-        <ProfileCard />
+        {user1Profile && (
+          <ProfileCard
+            imgSource={user1Profile.avatar_url}
+            name={user1Profile.name}
+            username={user1Profile.login}
+            followers={user1Profile.followers}
+            following={user1Profile.following}
+            joinedDate={user1Profile.created_at}
+            location={user1Profile.location}
+            office={user1Profile.company}
+          />
+        )}
         <div className={`self-stretch w-1 bg-gray-200`}></div>
-        <ProfileCard />
+        {user2Profile && (
+          <ProfileCard
+            imgSource={user2Profile.avatar_url}
+            name={user2Profile.name}
+            username={user2Profile.login}
+            followers={user2Profile.followers}
+            following={user2Profile.following}
+            joinedDate={user2Profile.created_at}
+            location={user2Profile.location}
+            office={user2Profile.company}
+          />
+        )}
       </div>
     </div>
   );
